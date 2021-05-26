b2be5ae4f3e2dfe6bbda010c785a5c7162e9ea18
esTechVMG <vicente.maroto@escuelaestech.es>
2021-05-26 17:21:11 +0200

    Start handling exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use App\Utils\Responses\IQResponse;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Prophecy\Exception\Doubler\MethodNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -35,7 +40,27 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+        });
+        $this->renderable(function (Throwable $e, $request) {
+            switch (true) {
+                case $e instanceof MethodNotFoundException:
+                    return IQResponse::errorResponse(Response::HTTP_NOT_FOUND);
+                case $e instanceof MethodNotAllowedHttpException:
+                case $e instanceof AuthenticationException:
+                    return IQResponse::errorResponse(Response::HTTP_FORBIDDEN);
+                default:
+                    return IQResponse::errorResponse(Response::HTTP_NOT_FOUND);
+            }
         });
     }
 }
+
+
+/*
+            switch (true){
+                case $e instanceof MethodNotAllowedHttpException:
+                    return IQResponse::errorResponse(Response::HTTP_FORBIDDEN,$e);
+                default:
+                    return IQResponse::errorResponse(Response::HTTP_NOT_FOUND);
+            }
+*/
