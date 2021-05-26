ac42ef3c998e5af356cf46408c73fe89803e7a61
esTechVMG <vicente.maroto@escuelaestech.es>
2021-05-26 21:40:57 +0200

    Rename $debugInfo to $errors. No longer for development

--- app/Utils/Responses/IQResponse.php
+++ app/Utils/Responses/IQResponse.php
@@ -9,16 +9,16 @@ class IQResponse{
     public int $code;
     public string $message;
     public mixed $data;
-    public mixed $debugInfo;
+    public mixed $errors;
 
-    public function __construct(int $code = Response::HTTP_NOT_FOUND, mixed $data = null, mixed $debugInfo = null){
+    public function __construct(int $code = Response::HTTP_NOT_FOUND, mixed $data = null, mixed $errors = null){
         $this->code = $code;
         $this->message = array_key_exists($code,IQResponse::$messageDescriptions) ? IQResponse::$messageDescriptions[$code] : "No Message";
         if (isset($data)){
             $this->data = $data;
         }
-        if (config('app.debug') == true && isset($debugInfo)){
-            $this->debugInfo = $debugInfo;
+        if (isset($errors)){
+            $this->errors = $errors;
         }
     }
 
