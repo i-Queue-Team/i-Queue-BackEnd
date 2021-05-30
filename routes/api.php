60a5334fab60518c27668a390cbe0cf309a04aea
Vicente Maroto <vicente.maroto@escuelaestech.es>
2021-05-30 02:35:19 +0200

    CommerceQueueUserController Create a nested controller which controls the commerce queue.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CommerceController;
+use App\Http\Controllers\CommerceQueueUserController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CurrentQueueController;
 use App\Http\Controllers\QueueVerifiedUsersController;
@@ -24,33 +25,14 @@ use App\Http\Controllers\TestController;
 Route::middleware('auth:api')->group(function () {
     //All secure URL's
     //Header Authorization example :Authorization= "Bearer 9|EFV7swhyHN6VHvT0YV8f3L5MGgCCbkU53NTvGT4I" or "Bearer token"
-    Route::apiResource('commerces',CommerceController::class)->only([
-        'index', // Listado negocios
-        'show', // Mostrar un negocio en base al id
-        'store',//Guardar un negocio
+    Route::apiResource('commerces',CommerceController::class);
+
+    Route::apiResource('commerces/{id}/queue',CommerceQueueUserController::class)->only([
+        'index',    // Mostrar info de la cola (entry check, etc...)
+        'show',     // Mostrar informacion del usuario en la cola
+        'store',    // Añadir usuario a la cola
+        'destroy',  // Eliminar usuario de la cola
     ]);
-
-    //add queue to queues
-    //http://localhost/i-Queue-BackEnd/public/api/currentqueues
-    Route::post('current-queues', [CurrentQueueController::class, 'store']);
-
-    //for testing purposes
-    //http://localhost/i-Queue-BackEnd/public/api/queue_verified_users
-    Route::get('queue-verified-users', [QueueVerifiedUsersController::class, 'index']);
-
-    //add user to queue
-    //http://localhost/i-Queue-BackEnd/public/api/queue_verified_users
-    Route::post('queue-verified-users', [QueueVerifiedUsersController::class, 'store']);
-
-    //delete user to queue
-    //http://localhost/i-Queue-BackEnd/public/api/queue_verified_users
-    Route::delete('queue-verified-users/{id}', [QueueVerifiedUsersController::class, 'delete']);
-    //check if user can enter establishment (tablet)
-    Route::get('queue-verified-users-check/{id}', [QueueVerifiedUsersController::class, 'entry_check']);
-    //check unser info (phone)
-    Route::get('queue-verified-users/{id}', [QueueVerifiedUsersController::class, 'info']);
-
-
     Route::apiResource('users',UserController::class)->only(['show','destroy']);
 
 });
@@ -58,5 +40,5 @@ Route::middleware('auth:api')->group(function () {
 //AUTH login/register
 Route::post("login", [UserController::class, 'login'])->name('login');
 Route::post("register", [UserController::class, 'register']);
-Route::apiResource("testing",TestController::class);
+//Route::apiResource("testing",TestController::class);
 
