3b1c00fe5cf45776031eb42e5dfe494247d71b1a
esTechVMG <vicente.maroto@escuelaestech.es>
2021-05-31 18:44:36 +0200

    Revert api route changes

--- routes/api.php
+++ routes/api.php
@@ -27,12 +27,26 @@ Route::middleware('auth:api')->group(function () {
     //Header Authorization example :Authorization= "Bearer 9|EFV7swhyHN6VHvT0YV8f3L5MGgCCbkU53NTvGT4I" or "Bearer token"
     Route::apiResource('commerces',CommerceController::class);
 
-    Route::apiResource('commerces/{id}/queue',CommerceQueueUserController::class)->only([
-        'index',    // Mostrar info de la cola (entry check, etc...)
-        'show',     // Mostrar informacion del usuario en la cola
-        'store',    // Añadir usuario a la cola
-        'destroy',  // Eliminar usuario de la cola
-    ]);
+    //add queue to queues
+    //http://localhost/i-Queue-BackEnd/public/api/currentqueues
+    Route::post('current-queues', [CurrentQueueController::class, 'store']);
+
+    //for testing purposes
+    //http://localhost/i-Queue-BackEnd/public/api/queue_verified_users
+    Route::get('queue-verified-users', [QueueVerifiedUsersController::class, 'index']);
+
+    //add user to queue
+    //http://localhost/i-Queue-BackEnd/public/api/queue_verified_users
+    Route::post('queue-verified-users', [QueueVerifiedUsersController::class, 'store']);
+
+    //delete user to queue
+    //http://localhost/i-Queue-BackEnd/public/api/queue_verified_users
+    Route::delete('queue-verified-users/{id}', [QueueVerifiedUsersController::class, 'delete']);
+    //check if user can enter establishment (tablet)
+    Route::get('queue-verified-users-check/{id}', [QueueVerifiedUsersController::class, 'entry_check']);
+    //check unser info (phone)
+    Route::get('queue-verified-users/{id}', [QueueVerifiedUsersController::class, 'info']);
+
     Route::apiResource('users',UserController::class)->only(['show','destroy']);
 
 });
