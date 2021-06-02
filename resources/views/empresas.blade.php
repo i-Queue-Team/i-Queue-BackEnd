21f3155e1da6c0e086969ee8f1860fc5981bf4cf
f48 <57172092+f48@users.noreply.github.com>
2021-06-02 20:41:38 +0200

    fixed footer views

--- resources/views/empresas.blade.php
+++ resources/views/empresas.blade.php
@@ -16,36 +16,39 @@
         min-height: 100vh;
         flex-direction: column;
     }
+
     main {
         flex: 1 0 auto;
     }
+
 </style>
 
-<body>
+<body class="container">
     <!--contenedor-->
-    <div class="container" style="min-width: 261px;">
-        <!--menu-->
 
-        <nav class="nav-extended">
-            <div class="nav-wrapper" style="margin-left: 8px;">
-                <a href="{{ url('/home') }}" class="brand-logo"><span class=".center-align">I-Queue</span></a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="{{ url('/login') }}"><img src="./images/userlogin.png"
-                                style=" padding: 5px 0px 5px 0px; margin-top: 10px;" alt=""></a></li>
-                </ul>
+    <!--menu-->
 
-                <a href="{{ url('/login') }}" data-target="mobile-demo" class="sidenav-trigger"
-                    style=" margin: 10px 0px 0px 0px; padding-left: 10px; height: 20px;"><img
-                        src="./images/userlogin.png" alt=""></a>
-            </div>
-            <div class="nav-content">
-                <ul class="tabs tabs-transparent">
-                    <li><a href="{{ url('/contactoempresas') }}">Empresas</a></li>
-                    <li><a href="{{ url('/sobrenosotros') }}">Sobre nosotros</a></li>
-                </ul>
-            </div>
-        </nav>
+    <nav class="nav-extended">
+        <div class="nav-wrapper" style="margin-left: 8px;">
+            <a href="{{ url('/home') }}" class="brand-logo"><span class=".center-align">I-Queue</span></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="{{ url('/login') }}"><img src="./images/userlogin.png"
+                            style=" padding: 5px 0px 5px 0px; margin-top: 10px;" alt=""></a></li>
+            </ul>
+
+            <a href="{{ url('/login') }}" data-target="mobile-demo" class="sidenav-trigger"
+                style=" margin: 10px 0px 0px 0px; padding-left: 10px; height: 20px;"><img src="./images/userlogin.png"
+                    alt=""></a>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent">
+                <li><a href="{{ url('/contactoempresas') }}">Empresas</a></li>
+                <li><a href="{{ url('/sobrenosotros') }}">Sobre nosotros</a></li>
+            </ul>
+        </div>
         <!--fin menu-->
+    </nav>
+    <main>
         <h2 class="center-align"><b>I-Queue Empresas</b></h2>
 
 
@@ -171,53 +174,54 @@
 
 
 
+    </main>
 
-            <!-- Footer -->
-            <footer class="page-footer">
-                <div class="container">
-                    <div class="row">
-                        <div class="col l6 s12">
-                            <h5 class="white-text">Info</h5>
-                            <p class="grey-text text-lighten-4"> I-Queue es la aplicación multiplataforma desarrollada
-                                por el equipo de la promoción 2020-2021 de Escuela Estech del Grado Superior en
-                                Desarrollo de Aplicaciones multiplataforma. La idea está basada en la digitalización,
-                                monitorización y adaptación de la misma para una gran cantidad de modelos de negocio de
-                                cara a la mejora e implementación de sus servicios de cara al público.</p>
-                        </div>
-                        <div class="col l4 offset-l2 s12">
-                            <h5 class="white-text">Descárgate la app</h5>
-                            <ul>
-                                <li><a class="grey-text text-lighten-3" href="#!"><img src="./images/appstore.PNG"
-                                            alt=""></a></li>
-                                <br>
-                                <li><a class="grey-text text-lighten-3" href="#!"><img src="./images/androidstore.PNG"
-                                            alt=""></a></li>
-                                <li>
-                                    <h5>Redes sociales</h5>
-                                    <a class="grey-text text-lighten-3" href="#!"><img src="./images/facebook.png"
-                                            alt=""></a>
-                                    <a class="grey-text text-lighten-3" href="#!"><img
-                                            src="./images/correoelectronico.png" alt=""></a>
-                                    <a class="grey-text text-lighten-3" href="#!"><img src="./images/gorjeo.png"
-                                            alt=""></a>
-                                    <a class="grey-text text-lighten-3" href="#!"><img src="./images/instagram.png"
-                                            alt=""></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+
+
+
+    <!-- Footer -->
+    <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Info</h5>
+                    <p class="grey-text text-lighten-4"> I-Queue es la aplicación multiplataforma desarrollada
+                        por el equipo de la promoción 2020-2021 de Escuela Estech del Grado Superior en
+                        Desarrollo de Aplicaciones multiplataforma. La idea está basada en la digitalización,
+                        monitorización y adaptación de la misma para una gran cantidad de modelos de negocio de
+                        cara a la mejora e implementación de sus servicios de cara al público.</p>
                 </div>
-                <div class="footer-copyright">
-                    <div class="container">
-                        <p class="center-align">© 2021 Copyright I-Queue team</p>
-                    </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Descárgate la app</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!"><img src="./images/appstore.PNG" alt=""></a>
+                        </li>
+                        <br>
+                        <li><a class="grey-text text-lighten-3" href="#!"><img src="./images/androidstore.PNG"
+                                    alt=""></a></li>
+                        <li>
+                            <h5>Redes sociales</h5>
+                            <a class="grey-text text-lighten-3" href="#!"><img src="./images/facebook.png" alt=""></a>
+                            <a class="grey-text text-lighten-3" href="#!"><img src="./images/correoelectronico.png"
+                                    alt=""></a>
+                            <a class="grey-text text-lighten-3" href="#!"><img src="./images/gorjeo.png" alt=""></a>
+                            <a class="grey-text text-lighten-3" href="#!"><img src="./images/instagram.png" alt=""></a>
+                        </li>
+                    </ul>
                 </div>
-            </footer>
-            <!--fin footer-->
-
-
+            </div>
         </div>
-        <!--fin contenedor-->
+        <div class="footer-copyright">
+            <div class="container">
+                <p class="center-align">© 2021 Copyright I-Queue team</p>
+            </div>
+        </div>
+    </footer>
+    <!--fin footer-->
+
+
+    </div>
+    <!--fin contenedor-->
 </body>
 
 
