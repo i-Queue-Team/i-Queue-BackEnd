a310e2670b506a7ea678167896760642436089cd
esTechVMG <vicente.maroto@escuelaestech.es>
2021-06-07 21:13:22 +0200

    Add QueueVerifiedUsersResource

---
+++ app/Http/Resources/QueueVerifiedUsersResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Commerce;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QueueVerifiedUsersResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        //$resource = $this->resource;
+        //$commerce = $resource->queue;
+        //$resource = new CommerceResource($commerce);
+        $commerce = new CommerceResource($this->queue->commerce);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'position' => $this->position,
+            'estimated_time' => $this->estimated_time,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'queue_id' => $this->queue_id,
+            'user_id' => $this->user_id,
+            'image' => $commerce->resource->image,
+        ];
+    }
+}
