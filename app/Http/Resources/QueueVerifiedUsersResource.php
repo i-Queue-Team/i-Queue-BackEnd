cff05c314bec0f326f191a2675973d5dccacac61
esTechVMG <vicente.maroto@escuelaestech.es>
2021-06-08 02:06:05 +0200

    image attribute workaround (possible laravel bug)

--- app/Http/Resources/QueueVerifiedUsersResource.php
+++ app/Http/Resources/QueueVerifiedUsersResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use App\Models\Commerce;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class QueueVerifiedUsersResource extends JsonResource
@@ -20,6 +21,7 @@ class QueueVerifiedUsersResource extends JsonResource
         //$commerce = $resource->queue;
         //$resource = new CommerceResource($commerce);
         //$commerce = new CommerceResource($this->queue->commerce);
+        $image = $this->queue->commerce->image ? url('/') . Storage::url('') . 'commerces/' . $this->queue->commerce->image : "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80";
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -29,7 +31,7 @@ class QueueVerifiedUsersResource extends JsonResource
             'updated_at' => $this->updated_at,
             'queue_id' => $this->queue_id,
             'user_id' => $this->user_id,
-            //'image' => $commerce->resource->image,
+            'image' => $image,
         ];
     }
 }
