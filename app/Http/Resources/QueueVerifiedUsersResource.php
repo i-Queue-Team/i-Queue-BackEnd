e29528398409470e511fcafdd5eeaf0d89d7a6cd
esTechVMG <vicente.maroto@escuelaestech.es>
2021-06-09 20:04:40 +0200

    Return commerce name with queue ser in resource

--- app/Http/Resources/QueueVerifiedUsersResource.php
+++ app/Http/Resources/QueueVerifiedUsersResource.php
@@ -24,7 +24,7 @@ class QueueVerifiedUsersResource extends JsonResource
         $image = $this->queue->commerce->image ? url('/') . Storage::url('') . 'commerces/' . $this->queue->commerce->image : "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80";
         return [
             'id' => $this->id,
-            'name' => $this->name,
+            'name' => $this->queue->commerce->name,
             'position' => $this->position,
             'estimated_time' => $this->estimated_time,
             'created_at' => $this->created_at,
