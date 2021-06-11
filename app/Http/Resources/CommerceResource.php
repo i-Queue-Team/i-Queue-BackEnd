9cc5b5049c5243ecfc0a3e661d461e0c6192d395
Vicente Maroto <vicente.maroto@escuelaestech.es>
2021-06-11 20:00:24 +0200

    Simplify image Url handling

--- app/Http/Resources/CommerceResource.php
+++ app/Http/Resources/CommerceResource.php
@@ -16,14 +16,13 @@ class CommerceResource extends JsonResource
      */
     public function toArray($request)
     {
-        $image = $this->image ? url('/') . Storage::url('') . 'commerces/' . $this->image : "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80";
         return [
             'id' => $this->id,
             'name' => $this->name,
             'latitude' => $this->latitude,
             'longitude' => $this->longitude,
             'info' => $this->info,
-            'image' => $image,
+            'image' => $this->imageUrl(),
             'queueInfo' => $this->queue,
         ];
     }
