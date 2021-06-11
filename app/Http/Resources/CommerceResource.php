ee92ea3a2a928cb09061e56954e4ee6bee670a9e
Vicente Maroto <vicente.maroto@escuelaestech.es>
2021-06-11 17:56:50 +0200

    Branch review (Untested)

--- app/Http/Resources/CommerceResource.php
+++ app/Http/Resources/CommerceResource.php
@@ -24,7 +24,7 @@ class CommerceResource extends JsonResource
             'longitude' => $this->longitude,
             'info' => $this->info,
             'image' => $image,
-            'queue' => $this->queue,
+            'queueInfo' => $this->queue,
         ];
     }
 }
