5706fd556de7f5e8eabff64c6ce1fec70ed08d8f
f48 <57172092+f48@users.noreply.github.com>
2021-06-13 01:39:24 +0200

    Front end Term And conditions / And custom scroll bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,4 @@
-require('./bootstrap');
+
 $(document).ready(function() {
     $('.sidenav').sidenav();
 });
--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -74,9 +74,9 @@
                                 <input type="checkbox" id="terminos" onclick="sendBtn()" />
 
                                 <span>He leído y acepto los
-                                    <a href="#Condiciones" style="font-size:10pt">términos y condiciones</a>
+                                    <a  data-target="terms" class="modal-trigger" style="font-size:10pt">términos y condiciones</a>
                                     <span style="font-size:12pt">y la
-                                        <a href="#Condiciones" style="font-size:10pt">política de privacidad</a>
+                                        <a data-target="terms" class="modal-trigger" style="font-size:10pt">política de privacidad</a>
                                         .</span>
                             </label>
                         </div>
@@ -91,17 +91,37 @@
 
                     </form>
                 </article>
+
             </div>
+
         </section>
         <!--fin login-->
         <br>
-    </main>
 
+    </main>
+  <!-- terminos y condiciones-->
+  <div id="terms" class="modal">
+    <div class="modal-content">
+      <h4>Terminos Y Condiciones/Politica de privacidad</h4>
+      <p>El presente Pol&iacute;tica de Privacidad establece los t&eacute;rminos en que <strong>Iqueue&nbsp;</strong>usa y protege la informaci&oacute;n que es proporcionada por sus usuarios al momento de utilizar su sitio web.</p>
+<p>Esta compa&ntilde;&iacute;a est&aacute; comprometida con la seguridad de los datos de sus usuarios.&nbsp;</p>
+<p>Cuando le pedimos llenar los campos de informaci&oacute;n personal con la cual usted pueda ser identificado, lo hacemos asegurando que s&oacute;lo se emplear&aacute; de acuerdo con los t&eacute;rminos de este documento. Sin embargo esta <strong>Pol&iacute;tica de Privacidad&nbsp;</strong>puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos revisar continuamente esta p&aacute;gina para asegurarse que est&aacute; de acuerdo con dichos cambios.&nbsp;</p>
+<p><strong>INFORMACION RECOGIDA</strong></p>
+<p>Informaci&oacute;n que es recogida Nuestro sitio web podr&aacute; recoger informaci&oacute;n personal por ejemplo: Nombre, informaci&oacute;n de contacto como su direcci&oacute;n de correo electr&oacute;nica e informaci&oacute;n demogr&aacute;fica. As&iacute; mismo cuando sea necesario podr&aacute; ser requerida informaci&oacute;n espec&iacute;fica para procesar alg&uacute;n pedido o realizar una entrega o facturaci&oacute;n. Uso de la informaci&oacute;n recogida Nuestro sitio web emplea la informaci&oacute;n con el fin de proporcionar el mejor servicio posible, particularmente para mantener un registro de usuarios, de pedidos en caso que aplique, y mejorar nuestros productos y servicios. Es posible que sean enviados correos electr&oacute;nicos peri&oacute;dicamente a trav&eacute;s de nuestro sitio con ofertas especiales, nuevos productos y otra informaci&oacute;n publicitaria que consideremos relevante para usted o que pueda brindarle alg&uacute;n beneficio, estos correos electr&oacute;nicos ser&aacute;n enviados a la direcci&oacute;n que usted proporcione y podr&aacute;n ser cancelados en cualquier momento. <strong>Iqueue&nbsp;</strong>est&aacute; altamente comprometido para cumplir con el compromiso de mantener su informaci&oacute;n segura. Usamos los sistemas m&aacute;s avanzados y los actualizamos constantemente para asegurarnos que no exista ning&uacute;n acceso no autorizado. Cookies Una cookie se refiere a un fichero que es enviado con la finalidad de solicitar permiso para almacenarse en su ordenador, al aceptar dicho fichero se crea y la cookie sirve entonces para tener informaci&oacute;n respecto al tr&aacute;fico web, y tambi&eacute;n facilita las futuras valencia cf noticias recurrente. Otra funci&oacute;n que tienen las cookies es que con ellas las web pueden reconocerte individualmente y por tanto brindarte el mejor servicio personalizado de su web. Nuestro sitio web emplea las cookies para poder identificar las p&aacute;ginas que son visitadas y su frecuencia. Esta informaci&oacute;n es empleada &uacute;nicamente para <strong>an&aacute;lisis&nbsp;</strong><strong>estad&iacute;stico&nbsp;</strong>y despu&eacute;s la informaci&oacute;n se elimina de forma permanente.&nbsp;</p>
+<p><strong>COOKIES</strong></p>
+<p>Usted puede eliminar las cookies en cualquier momento desde su ordenador. Sin embargo las cookies ayudan a proporcionar un mejor servicio de los sitios web, est&aacute;s no dan acceso a informaci&oacute;n de su ordenador ni de usted, a menos de que usted as&iacute; lo quiera y la proporcione directamente. Usted puede aceptar o negar el uso de cookies, sin embargo la mayor&iacute;a de navegadores aceptan cookies autom&aacute;ticamente pues sirve para tener un mejor servicio web. Tambi&eacute;n usted puede cambiar la configuraci&oacute;n de su ordenador para declinar las cookies. Si se declinan es posible que no pueda utilizar algunos de nuestros servicios. Enlaces a Terceros Este sitio web pudiera contener en laces a otros sitios que pudieran ser de su inter&eacute;s. Una vez que usted de clic en estos enlaces y abandone nuestra p&aacute;gina, ya no tenemos control sobre al sitio al que es redirigido y por lo tanto no somos responsables de los t&eacute;rminos o privacidad ni de la protecci&oacute;n de sus datos en esos otros sitios terceros. Dichos sitios est&aacute;n sujetos a sus propias pol&iacute;ticas de privacidad por lo cual es recomendable que los consulte para confirmar que usted est&aacute; de acuerdo con estas. Control de su informaci&oacute;n personal En cualquier momento usted puede restringir la recopilaci&oacute;n o el uso de la informaci&oacute;n personal que es proporcionada a nuestro sitio web. Cada vez que se le solicite rellenar un formulario, como el de alta de usuario, puede marcar o desmarcar la opci&oacute;n de recibir informaci&oacute;n por correo electr&oacute;nico. En caso de que haya marcado la opci&oacute;n de recibir nuestro bolet&iacute;n o publicidad usted puede cancelarla en cualquier momento. Esta compa&ntilde;&iacute;a no vender&aacute;, ceder&aacute; ni distribuir&aacute; la informaci&oacute;n personal que es recopilada sin su consentimiento, salvo que sea requerido por un juez con un orden judicial. <strong>Iqueue&nbsp;</strong>Se reserva el derecho de cambiar los t&eacute;rminos de la presente Pol&iacute;tica de Privacidad en cualquier momento. </p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">OK</a>
+    </div>
+  </div>
     @include('footerLayout')
 
 </body>
 
+
 <script id="envio">
+
     function sendBtn() {
         var terminos = document.getElementById("terminos")
         var sendBtn = document.getElementById("enviarregistro")
@@ -122,6 +142,9 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+<script>    $(document).ready(function(){
+    $('.modal').modal();
+  });</script>
 </body>
 
 </html>
