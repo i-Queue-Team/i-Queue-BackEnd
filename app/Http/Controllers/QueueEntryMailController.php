72e382dfe5b587dea5903f90e87c477b74ba9a57
f48 <57172092+f48@users.noreply.github.com>
2021-06-20 17:34:13 +0200

    Petition , only if mail is sent

--- app/Http/Controllers/QueueEntryMailController.php
+++ app/Http/Controllers/QueueEntryMailController.php
@@ -32,74 +32,78 @@ class QueueEntryMailController extends Controller
         //check if user is not in the database
         $userCheck = User::where('email', $request->email)->first();
 
-        if ($userCheck) {
-            //user not empty --> use this user
-            $user = $userCheck;
-        } else {
-            //user is empty -->> proceed to create user
-            $user = new User;
-            //user mail will be the one from request
-            $user->email = $request->email;
-            //username will be the email without domain
-            $user->name =  preg_replace("/[^a-zA-Z]+/", "", substr($request->email, 0, strrpos($request->email, '@')));
-            //random string password
-            $password = Str::random(10);
-            $user->password = Hash::make($password);
-            $user->save();
-        }
-        //if in queue already check
-        $queueUsers_check_inQueue = $user->queues->where('queue_id', '=', $request->queue_id);
-        if ($queueUsers_check_inQueue->count() > 0) {
-            return IQResponse::emptyResponse(Response::HTTP_CONFLICT);
-        }
-        //-----------------------------------------------
-        //save into queue
-        $queue = CurrentQueue::find($request->queue_id);
-
-        //queue instance
-        $queueVerifiedUser = new QueueVerifiedUser();
-        $queueVerifiedUser->queue_id = $queue->id;
-        $queueVerifiedUser->user_id = $user->id;
-        //posicion es igual a la funcion posicion
-        $queueVerifiedUser->position = $queue->positions();
-        //el tiempo estimado sera el actual con la adicion de los minutos recibidos de la funcion de tiempo estimado
-        $queueVerifiedUser->estimated_time = date('Y-m-d H:i:s');
-        $queueVerifiedUser->save();
-        $queue->refreshQueue();
-        $queue->storeStadistics($queueVerifiedUser);
-        //if user exist it was succesfull therefore we continue
-        if (!is_null($queueVerifiedUser)) {
-            $fromEmail = $user->email;
-            $fromName = 'Iqueue-Administration';
-            $commerceName = $queue->commerce->name;
-            //if password exists send password email
-            if (isset($password)) {
-                $data = array(
-                    'name' => $user->name,
-                    'email' => $fromEmail,
-                    'password' => $password,
-                    'commerceName' => $commerceName,
-                    'url'  => url("/login/" . $this::encrypt($fromEmail) . "/" . $this::encrypt($password)),
-                );
-                Mail::send('mail-with-password', $data, function ($message) use ($fromEmail, $fromName, $commerceName) {
-                    $message->to($fromEmail, $fromName)->subject("Bienvenido a i-Queue, Has entrado en " . $commerceName);
-                    $message->from(env('MAIL_USERNAME'), 'Iqueue-Administration');
-                });
-            } //if password doesnt exist send no password email
-            else {
-                $data = array(
-                    'name' => $user->name,
-                    'email' => $fromEmail,
-                    'commerceName' => $queue->commerce->name,
-                    'url'  => url("/login/" . $this::encrypt($fromEmail)),
-                );
-                Mail::send('mail-without-password', $data, function ($message) use ($fromEmail, $fromName, $commerceName) {
-                    $message->to($fromEmail, $fromName)->subject("Hola de nuevo, Has entrado en " . $commerceName);
-                    $message->from(env('MAIL_USERNAME'), 'Iqueue-Administration');
-                });
+        try {
+            if ($userCheck) {
+                //user not empty --> use this user
+                $user = $userCheck;
+            } else {
+                //user is empty -->> proceed to create user
+                $user = new User;
+                //user mail will be the one from request
+                $user->email = $request->email;
+                //username will be the email without domain
+                $user->name =  preg_replace("/[^a-zA-Z]+/", "", substr($request->email, 0, strrpos($request->email, '@')));
+                //random string password
+                $password = Str::random(10);
+                $user->password = Hash::make($password);
+                $user->save();
             }
-            return IQResponse::response(Response::HTTP_CREATED, new QueueVerifiedUsersResource($queueVerifiedUser));
-        } else {
+            //if in queue already check
+            $queueUsers_check_inQueue = $user->queues->where('queue_id', '=', $request->queue_id);
+            if ($queueUsers_check_inQueue->count() > 0) {
+                return IQResponse::emptyResponse(Response::HTTP_CONFLICT);
+            }
+            //-----------------------------------------------
+            //save into queue
+            $queue = CurrentQueue::find($request->queue_id);
+
+            //queue instance
+            $queueVerifiedUser = new QueueVerifiedUser();
+            $queueVerifiedUser->queue_id = $queue->id;
+            $queueVerifiedUser->user_id = $user->id;
+            //posicion es igual a la funcion posicion
+            $queueVerifiedUser->position = $queue->positions();
+            //el tiempo estimado sera el actual con la adicion de los minutos recibidos de la funcion de tiempo estimado
+            $queueVerifiedUser->estimated_time = date('Y-m-d H:i:s');
+            $queueVerifiedUser->save();
+            $queue->refreshQueue();
+            $queue->storeStadistics($queueVerifiedUser);
+            //if user exist it was succesfull therefore we continue
+            if (!is_null($queueVerifiedUser)) {
+                $fromEmail = $user->email;
+                $fromName = 'Iqueue-Administration';
+                $commerceName = $queue->commerce->name;
+                //if password exists send password email
+                if (isset($password)) {
+                    $data = array(
+                        'name' => $user->name,
+                        'email' => $fromEmail,
+                        'password' => $password,
+                        'commerceName' => $commerceName,
+                        'url'  => url("/login/" . $this::encrypt($fromEmail) . "/" . $this::encrypt($password)),
+                    );
+                    Mail::send('mail-with-password', $data, function ($message) use ($fromEmail, $fromName, $commerceName) {
+                        $message->to($fromEmail, $fromName)->subject("Bienvenido a i-Queue, Has entrado en " . $commerceName);
+                        $message->from(env('MAIL_USERNAME'), 'Iqueue-Administration');
+                    });
+                } //if password doesnt exist send no password email
+                else {
+                    $data = array(
+                        'name' => $user->name,
+                        'email' => $fromEmail,
+                        'commerceName' => $queue->commerce->name,
+                        'url'  => url("/login/" . $this::encrypt($fromEmail)),
+                    );
+                    Mail::send('mail-without-password', $data, function ($message) use ($fromEmail, $fromName, $commerceName) {
+                        $message->to($fromEmail, $fromName)->subject("Hola de nuevo, Has entrado en " . $commerceName);
+                        $message->from(env('MAIL_USERNAME'), 'Iqueue-Administration');
+                    });
+                }
+                return IQResponse::response(Response::HTTP_CREATED, new QueueVerifiedUsersResource($queueVerifiedUser));
+            } else {
+                return IQResponse::emptyResponse(Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+        } catch (\Swift_TransportException $e) {
             return IQResponse::emptyResponse(Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
